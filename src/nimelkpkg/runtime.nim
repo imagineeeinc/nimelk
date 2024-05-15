bbb4e03fcac5bdd975fe1f9fcc9230dfdeeff301
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 <59028351+imagineeeinc@users.noreply.github.com>
2024-05-15 20:10:34 +0100

    added print function

--- src/nimelkpkg/runtime.nim
+++ src/nimelkpkg/runtime.nim
@@ -1,13 +1,13 @@
 import elk
 
-proc js_print(js: ptr js, args: jsval_t, nargs: cint): jsval_t {.cdecl.} =
-  var a: jsval_t = args
-  echo a
-  return js_mktrue().jsval_t
+proc js_print(js: ptr js, args: ptr jsval_t, nargs: cint): jsval_t {.cdecl procvar.}=
+  let val: jsval_t = args[]
+  echo js_str(js, val)
+  return js_mkundef()
 
 proc initRuntime*(code: string): cstring =
   var mem: array[2048, char]
   var js = js_create(mem.addr, uint(sizeof(mem)))
-  #js_set(js, js_glob(js), "print", js_mkfun(js_print))
+  js_set(js, js_glob(js), "print", js_mkfun(js_print));
   var v: jsval_t = js_eval(js, code, uint(len(code)))
-  return js_str(js, v)
+  return "OK: " & $(js_str(js, v))
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
 let a = 1;
 let b = 2;
 let c = a + b;
+print(c);
 a+b >= c ? true : false;
